016b7230527440e419dfcd7d39bfbda55f592f0e
marinabrovko <marishka.brovko@gmail.com>
2024-08-28 00:11:13 +0300

    №5

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-DATA_DIR = BASE_DIR / 'data'
+DATA_DIR = BASE_DIR / "data"
 
-file_json_product = DATA_DIR / 'products.json'
+file_json_product = DATA_DIR / "products.json"
--- src/read_product.py
+++ src/read_product.py
@@ -1,13 +1,12 @@
 import json
 
-
-from src.product import Product
 from src.category import Category
 from src.config import file_json_product
+from src.product import Product
 
 
 def read_json(path: str) -> dict:
-    with open(path, 'r', encoding='utf-8') as file:
+    with open(path, "r", encoding="utf-8") as file:
         data = json.load(file)
     return data
 
@@ -16,15 +15,15 @@ def create_objects_from_json(data):
     categories = []
     for category in data:
         products = []
-        for product in category['products']:
+        for product in category["products"]:
             products.append(Product(**product))
-        category['products'] = products
+        category["products"] = products
         categories.append(Category(**category))
 
     return categories
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     info = read_json(file_json_product)
     json_product = create_objects_from_json(info)
     print(json_product)
