f4eefaa56908505e2efd3bedd6edae3813273932
marinabrovko <marishka.brovko@gmail.com>
2024-08-28 00:10:01 +0300

    №5

---
+++ test/test_product.py
@@ -0,0 +1,5 @@
+def test_product(product):
+    assert product.name == "55\" QLED 4K"
+    assert product.description == "Фоновая подсветка"
+    assert product.price == 123000.0
+    assert product.quantity == 7
