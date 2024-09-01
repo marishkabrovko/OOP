a7646cbeba73717c5fac9aa1e701e7edd4d4ed6f
marinabrovko <marishka.brovko@gmail.com>
2024-09-01 15:09:15 +0300

    #add test1

--- test/test_category.py
+++ test/test_category.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def test_category_init(first_category, second_category):
     assert first_category.name == "Смартфоны"
     assert first_category.description == (
@@ -13,7 +16,19 @@ def test_category_init(first_category, second_category):
 
 
 def test_category_products_property(first_category):
-    assert (
-        first_category.list_product
-        == "\n Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.\n Iphone 15, 210000.0 руб. Остаток: 8 шт.\n Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт."
-    )
+    assert (first_category.list_product == "\n Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.\n Iphone 15, 210000.0 руб. Остаток: 8 шт.\n Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт.")
+
+
+def test_category_str(first_category):
+    assert str(first_category) == "Смартфоны, колличество продуктов: 10 шт."
+
+
+def test_product_iterator(product_iterator):
+    iter(product_iterator)
+    assert product_iterator.index == 0
+    assert next(product_iterator).name == "Samsung Galaxy S23 Ultra"
+    assert next(product_iterator).name == "Iphone 15"
+    assert next(product_iterator).name == "Xiaomi Redmi Note 11"
+
+    with pytest.raises(StopIteration):
+        next(product_iterator)
